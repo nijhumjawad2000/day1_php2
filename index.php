314ca9abdd53fdd154dfd2c6b8ea37f570230dd7
nijhum_jawad2000 <njawad@techlaunch.io>
2019-01-08 15:20:47 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+$url = "localhost/data.php?e=hello,world,how,are,you";
+
+$parts = parse_url($url);
+parse_str($parts['query'], $query);
+$array = (explode(',',$query['e']));
+foreach ($array as $item) {
+   echo "<li>$item</li>";
+}
+?>
